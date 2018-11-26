c74adf1e82fbf933aef8f9795229f3251d7a9130
yoraish <yorai.shaoul@gmail.com>
2018-11-26 04:27:25 +0000

    first working(?) implementation

--- insta.py
+++ insta.py
@@ -7,6 +7,8 @@ from time import sleep
 import time
 import requests
 import datetime
+import os
+import glob
 
 
 def makeVideo(fps, imageList):
@@ -33,7 +35,7 @@ def init_timelapse(slow_factor = 2):
     global fps
     fps = 30 # fixed
     defWait = 1/fps # fixed
-    slowFactor = slow_factor # changeable
+    slowFactor = slow_factor # changeable from init --- acceleration should be more than X21, otherwise just X21
     wait = defWait*slowFactor # fixed, in seconds
     global wait_time_delta
     wait_time_delta = datetime.timedelta(seconds = wait)
@@ -49,20 +51,39 @@ def init_timelapse(slow_factor = 2):
 
     global camera 
     camera = PiCamera()
+    camera.vflip = True
     global last_capture_time 
     last_capture_time = datetime.datetime.now() - datetime.timedelta(days = 1)
-    global image_couter 
-    image_couter = 0
+    global image_counter 
+    image_counter = 0
+
+    # clear the folder where we will store images
+    files = glob.glob('photos/*.jpg')
+    for f in files:
+        os.remove(f)
+
+
+def get_sunrise_time():
+    '''returns a datetime object with the sunrise time in UTC
+    '''
+    r = requests.get('https://api.sunrise-sunset.org/json?lat=42.361145&lng=-71.057083')
+    time_string = r.json()['results']['sunrise']
+    time_lst= time_string.split(':')
+    hour = int(time_lst[0])
+    minute = int(time_lst[1])
+
+    # set so it is the next day
+    start_time_utc = datetime.datetime.now() + datetime.timedelta(days = 1)
+    start_time_utc = start_time_utc.replace(hour = hour, minute = minute, second = 0)
+
+    # set to be 45 minutes before sunrise
+    start_time_utc = start_time_utc - datetime.timedelta(minutes = 45)
+    return start_time_utc
+
 
 
 
 last_update_time = datetime.datetime.now() - datetime.timedelta(minutes = 3)
-
-
-
-
-
-
 
 
 
@@ -95,13 +116,15 @@ if __name__ == "__main__":
         # gather info from the web
         if state == 0:
 
-            start_hour = 21
-            start_min = 50
-            start_sec = 30
+            start_time_utc = get_sunrise_time()
 
-            start_time = datetime.datetime.now()
-            start_time = start_time.replace(hour = start_hour, minute = start_min, second = 0)
-            start_time_utc = start_time + datetime.timedelta(hours = 5) - datetime.timedelta(days = 1)
+            # start_hour = 23
+            # start_min = 3
+            # start_sec = 30
+
+            # start_time = datetime.datetime.now()
+            # start_time = start_time.replace(hour = start_hour, minute = start_min, second = 0)
+            # start_time_utc = start_time + datetime.timedelta(hours = 5) - datetime.timedelta(days = 1)
 
             print('start time' ,start_time_utc)
             print('time now', datetime.datetime.now())
@@ -109,7 +132,7 @@ if __name__ == "__main__":
             if start_time_utc < datetime.datetime.now():
                 # the time now is AFTER the start time. that's bad.
                 post_server('ERROR - START TIME ' + str(start_time) +  ' ALREADY PASSED')
-                raise InterruptedError("ERROR - START TIME ' + str(start_time) +  ' ALREADY PASSED")
+                raise InterruptedError('ERROR - START TIME ' + str(start_time) +  ' ALREADY PASSED')
                 
             else:
                 state = 1
@@ -129,7 +152,7 @@ if __name__ == "__main__":
             # check the time
             if datetime.datetime.now() >= start_time_utc:
                 post_server('TIME TO START FILMING')
-                init_timelapse()
+                init_timelapse(1)
                 state = 3
             
             else:
@@ -141,25 +164,33 @@ if __name__ == "__main__":
         if state == 3:
             # take photos
             # check that we still have photos to take and that enough time passed since last capture
-            if image_couter < numImages and datetime.datetime.now() >= last_capture_time + wait_time_delta:
-
-                camera.capture('/home/pi/Desktop/instapy/photos/' +str(image_couter)+ '.jpg')
-                print('pic num', str(index))
+            if image_counter < numImages and datetime.datetime.now() >= last_capture_time + wait_time_delta:
+                print('pic num', str(image_counter))
+                camera.capture('/home/pi/instapy/photos/' +str(image_counter)+ '.jpg')
+                
                 last_capture_time = datetime.datetime.now()
+                image_counter += 1
 
             if datetime.datetime.now() >= last_update_time + datetime.timedelta(minutes = 1):
 
-                post_server("CAPTURING, DONE SOON")
+                post_server("CAPTURING,  " + str(image_counter)  +' / ' + str(numImages))
                 last_update_time = datetime.datetime.now()
             
-            if numImages == image_couter:
+            if numImages == image_counter:
                 # we captured all the photos, make a video and fall down to next state
+                state = 4
 
-                # create the image list
-                imageList = []
-                for index in range(numImages):
-                    img = cv2.imread('/home/pi/Desktop/instapy/photos/'+ str(index) +'.jpg')
-                    imageList.append(img)
+        if state == 4:
+            print('STATE 4')
 
-                # create the video
-                makeVideo(fps, imageList)
+            # create the image list
+            imageList = []
+            for index in range(numImages):
+                img = cv2.imread('/home/pi/instapy/photos/'+ str(index) +'.jpg')
+                imageList.append(img)
+
+            # create the video
+            makeVideo(fps, imageList)
+            post_server("Done! In"  + str( datetime.datetime.now() - start_time_utc ) )
+
+            break
